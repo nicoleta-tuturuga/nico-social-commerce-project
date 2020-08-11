56dc694e6143b8e261db8329e4d017c1f14c54aa
nicoleta-tuturuga <nicoleta.tuturuga@yahoo.com>
2020-08-11 19:15:08 +0300

    feat(navigation): refactor navbar layout, add dynamic highlight on routes (10)

--- social-commerce-frontend/src/navs/Navigation/Navigation.js
+++ social-commerce-frontend/src/navs/Navigation/Navigation.js
@@ -1,224 +1,102 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Nav from 'react-bootstrap/Nav';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import classes from './Navigation.css';
+
+import './Navigation.css';
 
 import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 
 const Navigation = () => {
   return (
 
-    <div>
-
+    <div className="navigation-container">
 
       <header className="header-global">
-
-        <nav className="navbar navbar-expand-sm navbar-light flex-column align-items-strech" style={{ backgroundColor: '#ffffff' }}>
-          <div className="w-100 d-flex">
-            <a className="navbar-brand mx-sm-auto mr-auto" href="#">INN FASHION</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu2">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+        <ul className="settings-menu d-flex row justify-content-between">
+          <div className="list-services d-flex">
+            <li className="settings-menu-bar">
+              <button>Serviciu Clienti</button>
+            </li>
+            <li className="settings-menu-bar">
+              <button>Newsletter</button>
+            </li>
           </div>
+          <div className="session-menu d-flex">
+            <li>
+              <Link to="/logIn">
+                <button>Autentificare</button>
+              </Link>
+            </li>
+            <li>
+              <button>Favorite</button>
+            </li>
+            <li>
+              <button>Cos de cumparaturi</button>
+            </li>
+          </div>
+        </ul>
+        <nav className="navbar navbar-expand-sm flex-column align-items-strech" style={{ backgroundColor: '#ffffff' }}>
+          <button className="brand mx-sm-auto">INN FASHION</button>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu">
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div className="collapse navbar-collapse w-100" id="navbarMenu">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/">
-                  <button className="nav-link active">ACASA</button>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/allShopsPage">
-                  <button className="nav-link">MAGAZINE</button>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">FEMEI</a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">BARBATI</a>
-              </li>
-            </ul>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <form className="form-inline my-2 my-lg-0">
-                  <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                  <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-              </li>
-            </ul>
-          </div>
-          <div className="collapse navbar-collapse w-100" id="navbarMenu2">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/newsfeed">
-                  <button className="nav-link">NOUTATI
-                    <span className="sr-only">(current)</span>
+            <div>
+              <ul className="navbar-nav">
+                <NavLink exact={true} activeClassName='is-active' className="white-border" to="/">
+                  <li className="nav-item">
+                    <button>ACASA</button>
+                  </li>
+                </NavLink>
+                <NavLink activeClassName='is-active' className="white-border" to="/allShopsPage">
+                  <li className="nav-item">
+                    <button>MAGAZINE</button>
+                  </li>
+                </NavLink>
+                <li className="nav-item">
+                  <button>FEMEI</button>
+                </li>
+                <li className="nav-item">
+                  <button>BARBATI</button>
+                </li>
+                <li className="nav-item ml-auto">
+                  <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </li>
+              </ul>
+              <ul className="navbar-nav">
+                <NavLink activeClassName='is-active' className="white-border" to="/newsfeed">
+                  <li className="nav-item">
+                    <button>NOUTATI</button>
+                  </li>
+                </NavLink>
+                <li className="nav-item">
+                  <button>IMBRCAMINTE</button>
+                </li>
+                <li className="nav-item dropdown">
+                  <button className="dropdown-toggle" id="#navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    ACCESORII VINTAGE & INCALTAMINTE
                   </button>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">IMBRCAMINTE</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="#navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  ACCESORII VINTAGE & INCALTAMINTE
-        </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="#">Action</a>
-                  <a className="dropdown-item" href="#">Another action</a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">VESELA VINTAGE</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">OBIECTE DE MOBILIER</a>
-              </li>
-            </ul>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <button className="dropdown-item" >Action</button>
+                    <button className="dropdown-item" >Another action</button>
+                    <div className="dropdown-divider"></div>
+                    <button className="dropdown-item" >Something else here</button>
+                  </div>
+                </li>
+                <li className="nav-item">
+                  <button>VESELA VINTAGE</button>
+                </li>
+                <li className="nav-item">
+                  <button>OBIECTE DE MOBILIER</button>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
-
-
-
-        {/*<div className="list_services">
-      <ul className="settings_menu">
-        <li className="settings_menu_bar">
-          <a href="#">Serviciu Clienti</a>
-        </li>
-          <span> </span>
-        <li className="settings_menu_bar">
-          <a href="#">Newsletter</a>
-        </li>
-      </ul>
-  </div>
-
-<nav className="navbar navbar-expand-sm navbar-light flex-column align-items-stretch " style={{backgroundColor: '#ffffff'}}>    
-  <div className="brand_logo_section d-flex">
-      <a className="navbar-brand-logo mx-sm-auto mr-auto" href="#">
-        <img src="https://images.unsplash.com/photo-1507120410856-1f35574c3b45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-        className="navBrandLogo"></img>
-        INN FASHION
-        </a>
-    </div>
-
-    <div className="topBar_options">
-      <ul className="navbar-nav topBar_sections">
-        <li className="topBar_sections_options topBar_sections_options-active">
-          <a href="#">BOUTIQUES</a>
-        </li>
-        <li className="topBar_sections_options">
-          <a href="#">WOMEN</a>
-        </li>
-        <li className="topBar_sections_options">
-          <a href="#">MEN</a>
-        </li>
-        <li className="topBar_sections_options">
-          <a href="#">FARFURII</a>
-        </li>
-      </ul>
-      <ul className="navbar-nav ml-auto"> 
-        <li className="topBar_sections_options">
-        <form className="form-inline my-2 my-lg-0 w-100">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-        </li>
-     
-     </ul> 
-  
-
-
-   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-     <span className="navbar-toggler-icon"></span>
-  </button>
-
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">NOUTATI <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">IMBRCAMINTE</a>
-      </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          ACCESORII VINTAGE & INCALTAMINTE
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">VESELA VINTAGE</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">OBIECTE DE MOBILIER</a>
-      </li>
-    </ul>
-
-
-    </div>
-  </div>
-      </nav> */}
-        {/* </div> */}
       </header>
-    </div>
-
-
-
-
-
-
-
-
-
-
-    //  <div>
-    //   <div>
-    //     <ul className="leftSided">
-    //       <li className="lileftSided">Help</li>
-    //       <li className="lileftSided">Language</li>
-    //     </ul>
-    //   </div>
-
-    // <Navbar className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#ffffff'}}> 
-    //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //   <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="mr-auto">
-    //       <Nav.Link href="about">About</Nav.Link>
-    //       <NavDropdown title="More" id="basic-nav-dropdown">
-    //         <NavDropdown.Item href="about">About</NavDropdown.Item>
-    //         <NavDropdown.Divider />
-    //         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-    //       </NavDropdown>
-    //     </Nav>
-    //     <Navbar.Brand href="/">
-    //        <img src="https://images.unsplash.com/photo-1523115191856-c203e76215a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=401&q=80"  
-    //        className="navBrandImg" />
-    //       INN FASHION
-    //       </Navbar.Brand>
-    //     <Form inline>
-    //       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-    //       <Button variant="outline-success">Search</Button>
-    //     </Form>
-    //   </Navbar.Collapse>
-    // </Navbar>
-
-    // </div> 
+    </div >
   );
 };
 
