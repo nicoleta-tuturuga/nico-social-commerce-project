91263b3203fe89b659480de77896dc651e76b910
nicoleta-tuturuga <nicoleta.tuturuga@yahoo.com>
2020-10-12 14:46:05 +0300

    feat(seller-page): create PositiveFeedback component, add logic and a common style file, which will be used for all feedback components (20)

--- social-commerce-frontend/src/pages/SellerPage/sellerDetailedSections/feedback/PositiveFeedback.js
+++ social-commerce-frontend/src/pages/SellerPage/sellerDetailedSections/feedback/PositiveFeedback.js
@@ -2,24 +2,39 @@ import React from "react";
 
 import data from './feedback-data/positive-feedback.json';
 
+import './CommonFeedbackStyle.css';
+
 const positiveFeedback = () => {
 
-  // const getPositiveFeedbackData = data.map((positiveData) => {
-
-  // })
+  const getPositiveFeedbackData = data.map((positiveData) => {
+    return (
+      <div className="d-flex">
+        <div className="col-md-2 feedback-img-container">
+          <img src={positiveData.productImg}></img>
+        </div>
+        <div className="col-md-6">
+          <p>{positiveData.productName}</p>
+          <p>{positiveData.clientComments}</p>
+          <img className="feedback-img-logo" src={positiveData.feedbackLogoImg}></img>
+        </div>
+        <div className="d-flex col-md-4">
+          <div>
+            <p>Cumparator</p>
+            <p>{positiveData.clientName}</p>
+          </div>
+          <div>
+            <p>Data</p>
+            <p>{positiveData.feedbackDate}</p>
+          </div>
+        </div>
+      </div>
+    )
+  })
 
 
   return (
     <div>
-      <img src=""></img>
-      <div>
-        <p>Some title here</p>
-        <p>Description here</p>
-      </div>
-      <div>
-        
-      </div>
-
+      {getPositiveFeedbackData}
     </div>
   )
 }
