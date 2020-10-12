6282c5f0ec018f24b18b5d5c975b82dfbaa4e35c
nicoleta-tuturuga <nicoleta.tuturuga@yahoo.com>
2020-10-12 15:14:33 +0300

    feat(seller-page): add logic to SellerFeedback page to navigate between the different types of feedback available (20)

--- social-commerce-frontend/src/pages/SellerPage/sellerDetailedSections/SellerFeedback.js
+++ social-commerce-frontend/src/pages/SellerPage/sellerDetailedSections/SellerFeedback.js
@@ -1,17 +1,67 @@
 import React from 'react';
 
-const feedback = () => {
+import PositiveFeedback from './feedback/PositiveFeedback';
+import AverageFeedback from './feedback/AverageFeedback';
+import NegativeFeedback from './feedback/NegativeFeedback';
 
-  return(
-    <div>
-      <p>Parerea clientilor</p>
-      <ul className="d-flex">
-        <li>Pozitiv</li>
-        <li>Mediu</li>
-        <li>Negativ</li>
-      </ul>
-    </div>
-  )
+const positiveFeedback = 'positive';
+const averageFeedback = 'average';
+const negativeFeedback = 'negative';
+
+class feedback extends React.Component {
+
+  state = {
+    activeFeedback: 'positive'
+  }
+
+  togglePositiveFeedback = () => {
+    this.setState({activeFeedback: positiveFeedback});
+  }
+
+  toggleAverageFeedback = () => {
+    this.setState({activeFeedback: averageFeedback});
+  }
+
+  toggleNegativFeedback = () => {
+    this.setState({activeFeedback: negativeFeedback});
+  }
+
+  render(){
+
+    let showActiveFeedback;
+    if(this.state.activeFeedback === positiveFeedback) {
+      showActiveFeedback = <PositiveFeedback />;
+    }
+    if(this.state.activeFeedback === averageFeedback) {
+      showActiveFeedback = <AverageFeedback />;
+    }
+    if(this.state.activeFeedback === negativeFeedback) {
+      showActiveFeedback = <NegativeFeedback />;
+    }
+
+    return (
+      <div>
+        <p>Parerea clientilor</p>
+        <ul className="d-flex">
+          <li>
+            <button onClick={this.togglePositiveFeedback}>Pozitiv</button>
+          </li>
+          <li>
+            <button onClick={this.toggleAverageFeedback}>Mediu</button>
+          </li>
+          <li>
+            <button onClick={this.toggleNegativFeedback}>Negativ</button>
+          </li>
+        </ul>
+
+        <div>
+          {showActiveFeedback}
+        </div>
+       
+      </div>
+    )
+  }
+
 }
 
 export default feedback;
